b1e757a8f39e44cc946d569309796f727a91f625
daniel <daniel.t.mortensen@gmail.com>
2022-06-28 18:09:52 -0600

    added final edits to letter for editor

--- main.m
+++ main.m
@@ -5,18 +5,20 @@ addpath('optimization/');
 addpath('visualization/');
 close all;
 ...fData = '\\wsl.localhost\Ubuntu\home\dmortensen\MILP_planning_paper\data';
-fData = 'data/';
+...fData = 'data/';
+fData = '/home/daniel/PhD/paper2/data_management/data/';
+fExternalLoad = fullfile('processed_tpss','TPSS_Cov15.mat'); ...'originalTpss1Day.csv';
 nBus = 5;
-nCharger = 5;
+nCharger = 1;
 
 % Define Scenarios
 scenario1 = getScenario('fExternalLoad','originalTpss1Day.csv',...
     'nBus',nBus,'nDayChargeRate',1,'nOverheadCharger',nCharger,...
-    'minObjective','weightedConsumption','fData',fData);
+    'minObjective','minSchedule8','fData',fData,'dTime',15);
 
 % run Scenarios
-saveFolder = 'resultsComparison';
-saveFile = '5_Bus_5_ChargeRate_1_Overhead_weightedConsumption';
+saveFolder = 'resultsOther';
+saveFile = '5_Bus_1_ChargeRate_1_Overhead_minSchedule8_dTime_15';
 savePath = fullfile(saveFolder, saveFile);
 result1 = runSimulation(scenario1, savePath);
 
